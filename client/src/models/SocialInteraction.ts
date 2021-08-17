b1871e1535918f216c8286d171664c80b9ea89b3
MAGENIC\daniellev <daniellev@magenic.com>
2021-08-17 16:35:06 +0800

    Convert class to interface

--- client/src/models/SocialInteraction.ts
+++ client/src/models/SocialInteraction.ts
@@ -1,18 +1,7 @@
-// TODO : Should I implement this as an interface instead?
-export class SocialInteraction {
-
+export interface SocialInteraction {
     _id: string;
     name : string;
     date: Date;
     hours: number;
     isSocialDistancing : boolean;
-
-    constructor() {
-        this._id = '';
-        this.name = '';
-        this.date = new Date();
-        this.hours = 0;
-        this.isSocialDistancing = false;
-    }
-
 }
--- client/src/models/VisitedPlace.ts
+++ client/src/models/VisitedPlace.ts
@@ -1,17 +1,7 @@
-export class VisitedPlace {
-
+export interface VisitedPlace {
     _id: string;
     place : string;
     date: Date;
     hours: number;
     isCrowded : boolean;
-
-    constructor() {
-        this._id = '';
-        this.place = '';
-        this.date = new Date();
-        this.hours = 0;
-        this.isCrowded = false;
-    }
-
 }
